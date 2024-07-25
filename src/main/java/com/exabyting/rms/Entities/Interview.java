35d504c3a0ca714b923d60aea4902649e613ce75
Md Ashikur Rahman <ashikurrahman@Mds-MacBook-Air.local>
2024-07-25 20:21:43 +0600

    ahss

--- src/main/java/com/exabyting/rms/Entities/Interview.java
+++ src/main/java/com/exabyting/rms/Entities/Interview.java
@@ -1,23 +1,23 @@
-//package com.exabyting.rms.Entities;
-//
-//import jakarta.persistence.*;
-//import lombok.*;
-//
-//import java.time.LocalDateTime;
-//import java.util.List;
-//
-//@Entity
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@Getter
-//@Setter
-//@Builder
-//public class Interview extends BaseEntity{
-//
-//    private LocalDateTime schedule;
-//    private String link;
-//    private String reportLink;
-//
+package com.exabyting.rms.Entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Interview extends BaseEntity{
+
+    private LocalDateTime schedule;
+    private String link;
+    private String reportLink;
+
 //    @ManyToMany
 //    @JoinTable(name = "interviewer" ,joinColumns = {@JoinColumn(name = "interview_id")}
 //    ,inverseJoinColumns = {@JoinColumn(name = "user_id")})
@@ -31,5 +31,5 @@
 //    @JoinColumn(name = "job_application_id")
 //    private JobApplication jobApplication;
 //
-//
-//}
+
+}
