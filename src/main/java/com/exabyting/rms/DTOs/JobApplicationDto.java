7c926a952c8170df3c90563c41d62dc0e32e4496
Md Ashikur Rahman <ashikurrahman@Mds-MacBook-Air.local>
2024-08-11 10:12:53 +0600

    ows

--- src/main/java/com/exabyting/rms/DTOs/JobApplicationDto.java
+++ src/main/java/com/exabyting/rms/DTOs/JobApplicationDto.java
@@ -2,6 +2,7 @@ package com.exabyting.rms.DTOs;
 
 import com.exabyting.rms.Entities.Helper.JobApplicationStatus;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,12 +19,10 @@ public class JobApplicationDto {
 //    @Pattern(regexp = "^(http|https|ftp)://([a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6})(:[0-9]{1,5})?(/[a-zA-Z0-9_.-]*)?$",
 //            message = "Invalid URL")
     private String socialUrl;
-    @NotBlank(message = "requirements is mandatory")
     private String resumeLink;
-    @NotBlank(message = "requirements is mandatory")
     private JobApplicationStatus status;
-    @NotBlank(message = "requirements is mandatory")
+    @NotNull(message = "jobOpeningId is mandatory")
     private Integer jobOpeningId;
-    @NotBlank(message = "requirements is mandatory")
+    @NotNull(message = "requirements is mandatory")
     private Integer userId;
 }
